e240add4276eff1384868400360d3b9bf58ca8fb
santisr05 <santiago.sepulveda4@udea.edu.co>
2025-03-17 15:38:53 -0500

    Se inicia la codificacion de la interfaz grafica ll

--- src/FrmDevuelta.java
+++ src/FrmDevuelta.java
@@ -1,9 +1,28 @@
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
 
 public class FrmDevuelta extends JFrame {
+
+    private JComboBox cmbDenominacion;
+    private int[] denominaciones = new int[]{100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50};
+
     public FrmDevuelta() {
         setTitle("Calular Devuelta");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        JLabel lblDenominacion = new JLabel("Denominacion");
+        lblDenominacion.setBounds(10, 10, 100, 25);
+        getContentPane().add(lblDenominacion);
+
+        cmbDenominacion = new JComboBox();
+        for (int denominacion : denominaciones) {
+            cmbDenominacion.addItem(denominacion);
+        }
+        cmbDenominacion.setBounds(110, 10, 100, 25);
+        getContentPane().add(cmbDenominacion);
     }
 }
