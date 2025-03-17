e40e199c23397fab5b2f3619979b73a313f6f64a
santisr05 <santiago.sepulveda4@udea.edu.co>
2025-03-17 15:36:51 -0500

    Se inicia la codificacion de la interfaz grafica

---
+++ src/FrmDevuelta.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class FrmDevuelta extends JFrame {
+    public FrmDevuelta() {
+        setTitle("Calular Devuelta");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
